a7f186cf7db5c231eb0a5a3e4e2098a691779027
Jerel Nielsen <jerel.nielsen@gmail.com>
2020-01-08 09:52:30 -0700

    plotter cleanup

--- scripts/plot_gimbal_ekf.m
+++ scripts/plot_gimbal_ekf.m
@@ -8,21 +8,22 @@ ekf_cov = reshape(fread(fopen(strcat(['/tmp/',params.name,'_ekf_cov.log']), 'r')
 
 figure()
 set(gcf, 'name', 'EKF Velocity', 'NumberTitle', 'off')
-titles = ["North", "East", "Down"];
+titles = ["Velocity North", "Velocity East", "Velocity Down"];
 idx = 1;
 for i=1:3
     subplot(3, 1, i), hold on, grid on
     title(titles(i))
-    plot(true_state(1,:), true_state(i + idx, :), 'linewidth', 2.0)
-    plot(ekf_state(1,:), ekf_state(i + idx, :), 'r-', 'linewidth', 1.5)
+    plot(true_state(1,:), true_state(i + idx, :), 'b-', 'linewidth', 2.0, 'DisplayName', 'Truth')
+    plot(ekf_state(1,:), ekf_state(i + idx, :), 'r-', 'linewidth', 1.5, 'DisplayName', 'EKF')
     if plot_cov == true
-        plot(ekf_state(1,:), ekf_state(i + idx, :) + 2 * sqrt(ekf_cov(i + idx, :)), 'm-', 'linewidth', 0.5)
-        plot(ekf_state(1,:), ekf_state(i + idx, :) - 2 * sqrt(ekf_cov(i + idx, :)), 'm-', 'linewidth', 0.5)
+        plot(ekf_state(1,:), ekf_state(i + idx, :) + 2 * sqrt(ekf_cov(i + idx, :)), 'r:', 'linewidth', 0.5, 'DisplayName', '2\sigma Bound')
+        plot(ekf_state(1,:), ekf_state(i + idx, :) - 2 * sqrt(ekf_cov(i + idx, :)), 'r:', 'linewidth', 0.5, 'HandleVisibility', 'off')
     end
     if i == 1
-        legend('Truth', 'EKF')
+        legend()
     end
     if i == 3
+        xlabel('Time (seconds)')
         set(gca, 'YDir', 'Reverse')
     end
 end
@@ -30,66 +31,68 @@ end
 
 figure()
 set(gcf, 'name', 'EKF Attitude', 'NumberTitle', 'off')
-titles = ["Roll (mrad)","Pitch (mrad)","Yaw (mrad)"];
+titles = ["Attitude Roll (mrad)","Attitude Pitch (mrad)","Attitude Yaw (mrad)"];
 idx = 4;
 for i=1:3
     subplot(3, 1, i), hold on, grid on
     title(titles(i))
-    plot(true_state(1,:), true_state(i + idx, :)*1000, 'linewidth', 2.0)
-    plot(ekf_state(1,:), ekf_state(i + idx, :)*1000, 'linewidth', 1.5)
+    plot(true_state(1,:), true_state(i + idx, :)*1000, 'b-', 'linewidth', 2.0, 'DisplayName', 'Truth')
+    plot(ekf_state(1,:), ekf_state(i + idx, :)*1000, 'r-', 'linewidth', 1.5, 'DisplayName', 'EKF')
     if plot_cov == true
-        plot(ekf_state(1,:), ekf_state(i + idx, :)*1000 + 2 * sqrt(ekf_cov(i + idx, :))*1000, 'm-', 'linewidth', 0.5)
-        plot(ekf_state(1,:), ekf_state(i + idx, :)*1000 - 2 * sqrt(ekf_cov(i + idx, :))*1000, 'm-', 'linewidth', 0.5)
+        plot(ekf_state(1,:), ekf_state(i + idx, :)*1000 + 2 * sqrt(ekf_cov(i + idx, :))*1000, 'r:', 'linewidth', 0.5, 'DisplayName', '2\sigma Bound')
+        plot(ekf_state(1,:), ekf_state(i + idx, :)*1000 - 2 * sqrt(ekf_cov(i + idx, :))*1000, 'r:', 'linewidth', 0.5, 'HandleVisibility', 'off')
     end
     if i == 1
-        legend('Truth', 'EKF')
+        legend()
+    end
+    if i == 3
+        xlabel('Time (seconds)')
     end
 end
-
-
-figure(), hold on, grid on
-set(gcf, 'name', 'EKF Accel Scale', 'NumberTitle', 'off')
-idx = 7;
-title("Accel Scale")
-plot(true_state(1,:), true_state(1 + idx, :), 'linewidth', 2.0)
-plot(ekf_state(1,:), ekf_state(1 + idx, :), 'linewidth', 1.5)
-if plot_cov == true
-    plot(ekf_state(1,:), ekf_state(1 + idx, :) + 2 * sqrt(ekf_cov(1 + idx, :)), 'm-', 'linewidth', 0.5)
-    plot(ekf_state(1,:), ekf_state(1 + idx, :) - 2 * sqrt(ekf_cov(1 + idx, :)), 'm-', 'linewidth', 0.5)
-end
-legend('Truth', 'EKF')
 
 
 figure()
 set(gcf, 'name', 'EKF Gyro Bias', 'NumberTitle', 'off')
-titles = ["x (mrad)","y (mrad)","z (mrad)"];
+titles = ["Gyro Bias X (mrad)","Gyro Bias Y (mrad)","Gyro Bias Z (mrad)"];
 idx = 8;
 for i=1:3
     subplot(3, 1, i), hold on, grid on
     title(titles(i))
-    plot(true_state(1,:), true_state(i + idx, :)*1000, 'linewidth', 2.0)
-    plot(ekf_state(1,:), ekf_state(i + idx, :)*1000, 'linewidth', 1.5)
+    plot(true_state(1,:), true_state(i + idx, :)*1000, 'b-', 'linewidth', 2.0, 'DisplayName', 'Truth')
+    plot(ekf_state(1,:), ekf_state(i + idx, :)*1000, 'r-', 'linewidth', 1.5, 'Displayname', 'EKF')
     if plot_cov == true
-        plot(ekf_state(1,:), ekf_state(i + idx, :)*1000 + 2 * sqrt(ekf_cov(i + idx, :))*1000, 'm-', 'linewidth', 0.5)
-        plot(ekf_state(1,:), ekf_state(i + idx, :)*1000 - 2 * sqrt(ekf_cov(i + idx, :))*1000, 'm-', 'linewidth', 0.5)
+        plot(ekf_state(1,:), ekf_state(i + idx, :)*1000 + 2 * sqrt(ekf_cov(i + idx, :))*1000, 'r:', 'linewidth', 0.5, 'DisplayName', '2\sigma Bound')
+        plot(ekf_state(1,:), ekf_state(i + idx, :)*1000 - 2 * sqrt(ekf_cov(i + idx, :))*1000, 'r:', 'linewidth', 0.5, 'HandleVisibility', 'off')
     end
     if i == 1
-        legend('Truth', 'EKF')
+        legend()
+    end
+    if i == 3
+        xlabel('Time (seconds)')
     end
 end
 
 
-figure(), hold on, grid on
-set(gcf, 'name', 'EKF Mag Bias', 'NumberTitle', 'off')
-idx = 11;
-title("Mag Bias (mrad)")
-plot(true_state(1,:), true_state(1 + idx, :)*1000, 'linewidth', 2.0)
-plot(ekf_state(1,:), ekf_state(1 + idx, :)*1000, 'linewidth', 1.5)
+figure()
+set(gcf, 'name', 'EKF Accel Scale and Mag Bias', 'NumberTitle', 'off')
+subplot(2, 1, 1), hold on, grid on
+title("Accel Scale")
+plot(true_state(1,:), true_state(1 + 7, :)*1000, 'b-', 'linewidth', 2.0, 'DisplayName', 'Truth')
+plot(ekf_state(1,:), ekf_state(1 + 7, :)*1000, 'r-', 'linewidth', 1.5, 'DisplayName', 'EKF')
 if plot_cov == true
-    plot(ekf_state(1,:), ekf_state(1 + idx, :)*1000 + 2 * sqrt(ekf_cov(1 + idx, :))*1000, 'm-', 'linewidth', 0.5)
-    plot(ekf_state(1,:), ekf_state(1 + idx, :)*1000 - 2 * sqrt(ekf_cov(1 + idx, :))*1000, 'm-', 'linewidth', 0.5)
+    plot(ekf_state(1,:), ekf_state(1 + 7, :)*1000 + 2 * sqrt(ekf_cov(1 + 7, :))*1000, 'r:', 'linewidth', 0.5, 'DisplayName', '2\sigma Bound')
+    plot(ekf_state(1,:), ekf_state(1 + 7, :)*1000 - 2 * sqrt(ekf_cov(1 + 7, :))*1000, 'r:', 'linewidth', 0.5, 'HandleVisibility', 'off')
 end
-legend('Truth', 'EKF')
+legend()
+subplot(2, 1, 2), hold on, grid on
+title("Mag Bias (mrad)")
+plot(true_state(1,:), true_state(1 + 11, :)*1000, 'b-', 'linewidth', 2.0, 'DisplayName', 'Truth')
+plot(ekf_state(1,:), ekf_state(1 + 11, :)*1000, 'r-', 'linewidth', 1.5, 'DisplayName', 'EKF')
+if plot_cov == true
+    plot(ekf_state(1,:), ekf_state(1 + 11, :)*1000 + 2 * sqrt(ekf_cov(1 + 11, :))*1000, 'r:', 'linewidth', 0.5, 'DisplayName', '2\sigma Bound')
+    plot(ekf_state(1,:), ekf_state(1 + 11, :)*1000 - 2 * sqrt(ekf_cov(1 + 11, :))*1000, 'r:', 'linewidth', 0.5, 'HandleVisibility', 'off')
+end
+xlabel('Time (seconds)')
 
 
 % if plot_pix_components
